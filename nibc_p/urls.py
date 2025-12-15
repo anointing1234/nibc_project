324aefc1e4a8631b636184ca447e6b996d415dff
Agmetrix <124325944+anointing1234@users.noreply.github.com>
2025-12-15 16:23:45 +0100

    Update urls.py

--- nibc_p/urls.py
+++ nibc_p/urls.py
@@ -23,7 +23,7 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('en/',include('accounts.urls')),
+    path('',include('accounts.urls')),
     path('Accounts/',include('accounts.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),

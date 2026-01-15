1ebab8d516d4f454ae74972c309487a7aeb36041
anointing1234 <yakubudestiny9@gmail.com>
2026-01-15 10:09:09 +0100

    commited

--- nibc_p/settings.py
+++ nibc_p/settings.py
@@ -183,17 +183,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+# Static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# if DEBUG:
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# else:
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Media files (IMPORTANT)
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "/var/media")
 
 
 # Default primary key field type
